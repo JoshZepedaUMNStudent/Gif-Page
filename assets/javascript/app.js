68091204ed2627c47ddd48d53abcaddae0a080a0
JZStudent <joshzepeda.student@gmail.com>
2018-06-17 09:15:23 -0500

    updated June 17

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 // Array for button types
 var carMake = ["acura", "bmw", "chrysler", "dodge", "fiat"];
 
+// Buttons for Gifs
 function renderButtons() {
 
     $("#cars-view").empty();
@@ -15,6 +16,7 @@ function renderButtons() {
     }
 }
 
+// Input Field for New Car
 $("#add-car").on("click", function(event) {
     event.preventDefault();
     var newCarMake = $("#car-input").val().trim();
@@ -23,36 +25,69 @@ $("#add-car").on("click", function(event) {
     renderButtons();
 });
 
-function showGifs() {
-    var car = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newCarMake +"key=TM1Ti97BZNzUM3NZraSrNAkGgwvIrkfi&limit=5" 
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    })  
-}
-
 renderButtons();
 
+// Displaying the Gifs
+var newCarMake = $("#car-input").val().trim();  //??????????
+var gueryURL = "http://api.giphy.com/v1/gifs/search?q=" + newCarMake + "&api_key=TM1Ti97BZNzUM3NZraSrNAkGgwvIrkfi&limit=5";
+ 
+$.ajax({
+    url: queryURL,
+    method:"GET"
+}).then(function(response) {
+    var tBody = $("tbody");
+    var tRow = $("<tr>");
 
-  
+    var columnOneTD = $("<td>").text(response);
+    var columnTwoTD = $("<td>").text(response);
+    var columnThreeTD = $("<td>").text(response);
+    var columnFourTD = $("<td>").text(response);
+    var columnFiveTD = $("<td>").text(response);
+    tRow.append(columnOneTD, columnTwoTD, columnThreeTD, columnFourTD, columnFiveTD);
+    tBody.append(tRow);
+});
 
-  
+
+
+
+
+
+
 // SAMPLE ONLY!
-// function displayGifs(){
-//     var action = $(this).attr("data-name");
-//     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
-//     console.log(queryURL); // displays the constructed url
+// var movie = "Mr. Nobody";
+// var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function(response) {
+//   var tBody = $("tbody");
+//   var tRow = $("<tr>");
+
+//   var titleTd = $("<td>").text(response.Title);
+//   var yearTd = $("<td>").text(response.Year);
+//   var actorsTd = $("<td>").text(response.Actors);
+//   tRow.append(titleTd, yearTd, actorsTd);
+//   tBody.append(tRow);
+// });
+
+// SAMPLE ONLY!
+// function showGifs() {
+//     var car = $(this).attr("data-name");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newCarMake +"key=TM1Ti97BZNzUM3NZraSrNAkGgwvIrkfi&limit=5" 
+//     console.log(queryURL);
 //     $.ajax({
 //         url: queryURL,
 //         method: 'GET'
-//     })
+//     })  
+// }
+
+  
+
+  
 
 
-// Add click listener to button on HTML page
-// Inside listener, create variable called "Button Name"
-// Target input box value
+
 
 
 
